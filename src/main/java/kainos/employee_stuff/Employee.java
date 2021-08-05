d3adb8ccc93089856479538e8524d9d18bc2943f
Thomas Cordall <thomas.cordall@kainos.com>
2021-08-05 10:49:46 +0100

    added attributes and constructor

--- src/main/java/kainos/employee_stuff/Employee.java
+++ src/main/java/kainos/employee_stuff/Employee.java
@@ -1,4 +1,19 @@
 package kainos.employee_stuff;
 
 public class Employee {
+    private String name;
+    private String address;
+    private String NI;
+    private int salary;
+    private int employeeNum;
+
+    public Employee(String name, String address, String NI, int salary, int employeeNum){
+        this.name = name;
+        this.address = address;
+        this.NI = NI;
+        this.salary = salary;
+        this.employeeNum = employeeNum;
+    }
+
+
 }
